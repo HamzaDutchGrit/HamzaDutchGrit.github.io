7f68c3c37624b07b8626ccf397513af5a4328375
Hamzq <hamza@dutchgrit.nl>
2024-10-07 14:50:45 +0200

    update

---
+++ webshop/script/script request.js
@@ -0,0 +1,5 @@
+document.getElementById('display-name').textContent = localStorage.getItem('name');
+document.getElementById('display-lastname').textContent = localStorage.getItem('lastname');
+document.getElementById('display-email').textContent = localStorage.getItem('email');
+document.getElementById('display-telephone').textContent = localStorage.getItem('telephone');
+document.getElementById('display-message').textContent = localStorage.getItem('message');
