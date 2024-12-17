6ae3fa9b422396bcf82cf998399d84c954d763f5
Hamzq <hamza@dutchgrit.nl>
2024-12-17 10:35:20 +0100

    Update api_links.js

--- header & footer/api_links.js
+++ header & footer/api_links.js
@@ -2,7 +2,7 @@
 
 // Create a global variable to store API base URLs that can be accessed throughout the application
 window.API_URL = {
-    accounts: 'https://localhost:7190/', 
+    accounts: 'https://login-api-psno.onrender.com/', 
     // Base URL for account-related API endpoints
 
     orders: 'https://localhost:7040/', 
